9f919f9f89f19ec2376469f6386a6e17624b7dbe
Sascha Weidner <support@sioweb.de>
2016-01-13 12:45:51 +0100

    f++

---
+++ assets/structure_ui.js
@@ -0,0 +1,15 @@
+(function($){
+
+$(document).on('rex:ready',function() {
+
+  $('.trigger_modal [data-modal]').click(function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    //var modal_id = $(this).attr('data-target');
+    $.get(url, function(data) {
+        $(data).modal();
+    });
+  });
+});
+
+})(jQuery);
